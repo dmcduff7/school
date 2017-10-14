fc8291252ce74133a14786dd594bdda9a346a728
dmcduff7 <32798417+dmcduff7@users.noreply.github.com>
2017-10-14 13:18:02 -0400

    Update Homework.java

--- Homework.java
+++ Homework.java
@@ -3,18 +3,36 @@
 /Section: 2
 /Name: Daniel McDuffie
 /Instructor: Yinka Akinwusi
-/Assignment:
-/Purpose:
+/Assignment: Homework 5
+/Purpose: Generate 4 random numbers.
 */
 
 import java.util.*;
 
-public class Homework {
+public class RandomNumbers {
 
   public static void main(String[] args) {
-  
-    //Create scanner.
-    Scanner scan = new Scanner(System.in);
     
-    //Ask user for input.
-    System.out.println("");
+    //Create random number generator.
+    Random rand = new Random();
+  
+    //Declare variables.
+    int num1, num2, num3;
+    double num4;
+    
+    //Create random number formulas.
+    
+    num1 = rand.nextInt(50) + 30;
+    num2 = rand.nextInt(20) + (-20);
+    num3 = rand.nextInt(-20) + (-60);
+    num4 = rand.nextDouble(15.9999) + 0.0;
+    
+    //Create output for the random numbers.
+    System.out.println("Random number between 30 and 50: //t" + num1 + ".");
+    System.out.println("Random number between -20 and 20: //t" + num2 + ".");
+    System.out.println("Random number between -60 and -20: //t" + num3 + ".");
+    System.out.println("Random number between 0.0 and 15.9999: //t" + num4 + ".");
+    
+  }
+  
+}
