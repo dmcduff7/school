c6a16227b9daf14c3d69823e8e7c58dc80f66460
dmcduff7 <32798417+dmcduff7@users.noreply.github.com>
2017-10-14 11:44:11 -0400

    Create Homework.java

---
+++ Homework.java
@@ -0,0 +1,20 @@
+/*
+/Class: CS 1301
+/Section: 2
+/Name: Daniel McDuffie
+/Instructor: Yinka Akinwusi
+/Assignment:
+/Purpose:
+*/
+
+import java.util.*;
+
+public class Homework {
+
+  public static void main(String[] args) {
+  
+    //Create scanner.
+    Scanner scan = new Scanner(System.in);
+    
+    //Ask user for input.
+    System.out.println("");
